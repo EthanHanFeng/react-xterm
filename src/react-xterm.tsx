4cf73d2fe6330d37feb7f4ca89ea48853873b8c5
Han Feng <hanf@seagroup.com>
2018-06-12 12:07:37 +0800

    addon require

--- src/react-xterm.tsx
+++ src/react-xterm.tsx
@@ -39,7 +39,7 @@ export default class XTerm extends React.Component<IXtermProps, IXtermState> {
     componentDidMount() {
         if (this.props.addons) {
             this.props.addons.forEach(s => {
-                const addon = require(`xterm/dist/addons/${s}/${s}`);
+                const addon = require(`xterm-ex/dist/addons/${s}/${s}`);
                 Terminal.applyAddon(addon);
             });
         }
