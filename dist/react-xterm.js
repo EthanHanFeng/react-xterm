2efd2d809a1b97fcbc2ea55abb6b829dba603dfb
Han Feng <hanf@seagroup.com>
2018-06-13 20:06:36 +0800

    build

--- dist/react-xterm.js
+++ dist/react-xterm.js
@@ -1,26 +1,38 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const xterm_ex_1 = require("xterm-ex");
+var React = require("react");
+var xterm_ex_1 = require("xterm-ex");
 exports.Terminal = xterm_ex_1.Terminal;
-const className = require('classnames');
-class XTerm extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.onInput = data => {
-            this.props.onInput && this.props.onInput(data);
+var className = require('classnames');
+var XTerm = (function (_super) {
+    __extends(XTerm, _super);
+    function XTerm(props, context) {
+        var _this = _super.call(this, props, context) || this;
+        _this.onInput = function (data) {
+            _this.props.onInput && _this.props.onInput(data);
         };
-        this.state = {
+        _this.state = {
             isFocused: false
         };
+        return _this;
     }
-    applyAddon(addon) {
+    XTerm.prototype.applyAddon = function (addon) {
         xterm_ex_1.Terminal.applyAddon(addon);
-    }
-    componentDidMount() {
+    };
+    XTerm.prototype.componentDidMount = function () {
         if (this.props.addons) {
-            this.props.addons.forEach(s => {
-                const addon = require(`xterm-ex/dist/addons/${s}/${s}`);
+            this.props.addons.forEach(function (s) {
+                var addon = require("xterm-ex/dist/addons/" + s + "/" + s);
                 xterm_ex_1.Terminal.applyAddon(addon);
             });
         }
@@ -37,60 +49,63 @@ class XTerm extends React.Component {
         if (this.props.value) {
             this.xterm.write(this.props.value);
         }
-    }
-    componentWillUnmount() {
+    };
+    XTerm.prototype.componentWillUnmount = function () {
         if (this.xterm) {
             this.xterm.destroy();
             this.xterm = null;
         }
-    }
-    shouldComponentUpdate(nextProps, nextState) {
+    };
+    XTerm.prototype.shouldComponentUpdate = function (nextProps, nextState) {
+        var _this = this;
         if (nextProps.hasOwnProperty('value') && nextProps.value != this.props.value) {
             if (this.xterm) {
                 this.xterm.clear();
-                setTimeout(() => {
-                    this.xterm.write(nextProps.value);
+                setTimeout(function () {
+                    _this.xterm.write(nextProps.value);
                 }, 0);
             }
         }
         return false;
-    }
-    getTerminal() {
+    };
+    XTerm.prototype.getTerminal = function () {
         return this.xterm;
-    }
-    write(data) {
+    };
+    XTerm.prototype.write = function (data) {
         this.xterm && this.xterm.write(data);
-    }
-    writeln(data) {
+    };
+    XTerm.prototype.writeln = function (data) {
         this.xterm && this.xterm.writeln(data);
-    }
-    focus() {
+    };
+    XTerm.prototype.focus = function () {
         if (this.xterm) {
             this.xterm.focus();
         }
-    }
-    focusChanged(focused) {
+    };
+    XTerm.prototype.focusChanged = function (focused) {
         this.setState({
             isFocused: focused
         });
         this.props.onFocusChange && this.props.onFocusChange(focused);
-    }
-    resize(cols, rows) {
+    };
+    XTerm.prototype.resize = function (cols, rows) {
         this.xterm && this.xterm.resize(Math.round(cols), Math.round(rows));
-    }
-    setOption(key, value) {
+    };
+    XTerm.prototype.setOption = function (key, value) {
         this.xterm && this.xterm.setOption(key, value);
-    }
-    refresh() {
+    };
+    XTerm.prototype.refresh = function () {
         this.xterm && this.xterm.refresh(0, this.xterm.rows - 1);
-    }
-    onContextMenu(e) {
+    };
+    XTerm.prototype.onContextMenu = function (e) {
         this.props.onContextMenu && this.props.onContextMenu(e);
-    }
-    render() {
-        const terminalClassName = className('ReactXTerm', this.state.isFocused ? 'ReactXTerm--focused' : null, this.props.className);
-        return React.createElement("div", { ref: ref => (this.container = ref), className: terminalClassName });
-    }
-}
-exports.default = XTerm;
+    };
+    XTerm.prototype.render = function () {
+        var _this = this;
+        var terminalClassName = className('ReactXTerm', this.state.isFocused ? 'ReactXTerm--focused' : null, this.props.className);
+        return React.createElement("div", { ref: function (ref) { return (_this.container = ref); }, className: terminalClassName });
+    };
+    return XTerm;
+}(React.Component));
 exports.XTerm = XTerm;
+exports.default = XTerm;
